d88187c7f723998da68b2523729fd69fe374f689
Klaudiusz <klaudiusz.s1405@gmail.com>
2023-09-01 18:42:47 +0200

    commit

--- roomba/bash_scripts/base.bash
+++ roomba/bash_scripts/base.bash
@@ -6,7 +6,7 @@ ign gazebo /home/klaudiusz/Documents/ros_roomba/src/roomba/gazebo/roomba_world.s
 # Launch RViz2 from config file
 rviz2 -d /home/klaudiusz/Documents/ros_roomba/src/roomba/rviz/config.rviz &
 
-sleep 2
+sleep 1
 
 # Run the Gazebo bridges
 ros2 run ros_gz_bridge parameter_bridge /imu@sensor_msgs/msg/Imu[ignition.msgs.IMU &
@@ -19,12 +19,12 @@ cd /home/klaudiusz/Documents/ros_roomba/src/roomba/webpage/
 # Run HTTP server on port 7000
 python -m http.server 7000 &
 
-sleep 2
+sleep 1
 
 # Run the web bridge
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
 
-sleep 2
+sleep 1
 
 # Open Chrome on port 7000
 google-chrome http://0.0.0.0:7000/ &
